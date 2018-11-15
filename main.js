b93bf40a94844a5adc2c67077bb111deb7df0024
LeyaHatoum <leyahatoum10@gmail.com>
2018-11-15 14:06:48 -0500

    just committin

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 //Create an object for the insults
 const gramseyInsults = {};
+const numbers = [] --> //use reduce method to add;
 
-//Create an array categorizing the general insults
-gramseyInsults.general = [];
 
 //Get API Key for GIPHY
 const myKey = "t9uzA10qm0ohWGiPoxU43lEDXVLCcj0s"
-const answer = $("input").val();
-const giphy = $.get(`http://api.giphy.com/v1/gifs/random?api_key=${myKey}&tag=${answer.toLowerCase()}&limit=1`);
 
 //Based on what is typed pick a randomized gif from Giphy
 gramseyInsults.formSubmit = function () {
   $("form").on("submit", function (event) {
     event.preventDefault();
     
+    //add answer to search query for Giphy
+    const answer = $("input").val();
+    const randomResult = function(){
+    return  math.Floor(math.Random() * 50)
+    };
+    console.log(randomResult());
+    const giphy = $.get(`http://api.giphy.com/v1/gifs/search?q=random+gordon+ramsay+frustrated+food&api_key=${myKey}&limit=1$offset=${randomResult()}`);
+    
+    //append the gif result to the div on the DOM
     giphy.done(function(data){
       console.log(data);
-      $("div").style.display = "block";
-      $("div").src = data.data.images.original.url;
+      const theGif = document.createElement("img");
+      const gifLocation = data.data[0].images.original.url;
+      console.log(gifLocation); 
+      console.log("TITLE:", data.data[0].title)
+      theGif.src = gifLocation;
+      $("div").append(theGif);
     });
 
-    // const generalInsult = gramseyInsults.general;
-    // let randomInsult = generalInsult[Math.floor(Math.random() * gramseyInsults.general.length)];
-    // console.log(randomInsult);
-    // const answer = $("input").val();
-    // $("div").append()
-    // $("div").append(randomInsult);
   });
 };
 
